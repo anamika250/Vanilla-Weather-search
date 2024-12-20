8069814c71342da54474f09af83f364363148d23
Anamika <anamika2004abc@gmail.com>
2024-12-20 13:36:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function refreshweather(response) {
+  console.log(response);
+}
+function searchCity(city) {
+  let apikey = "8714aed335fo8d04bt89a4b5c1266e26";
+  let apiurl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric"`;
+  axios.get(apiurl)
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
