0c6fad8501ffefc694918ac05dbecd4795cf5f01
Anamika <anamika2004abc@gmail.com>
2024-12-20 13:37:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-function refreshweather(response) {
-  console.log(response);
-}
 function searchCity(city) {
   let apikey = "8714aed335fo8d04bt89a4b5c1266e26";
   let apiurl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric"`;
-  axios.get(apiurl)
 }
 
 function handleSearchSubmit(event) {
